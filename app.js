6ee83e171aee1621dedb0ed15d19d4f844f62bbb
navya <navya.kurakula9@gmail.com>
2024-06-28 10:23:05 +0530

    Chapter1

---
+++ app.js
@@ -0,0 +1,50 @@
+
+    // const heading=React.createElement("h1",{id:"heading"},"Hello from React");
+    // console.log(heading);
+    // const h2=React.createElement("h2",{},"H2 element");
+    // // const root=ReactDOM.createRoot(document.getElementById("root"))
+    // root.render(heading);
+    // const listele=React.createElement("li",{},"orange","grapes");
+    // const root2=ReactDOM.createRoot(document.getElementById("list"));
+    // root2.render(listele);
+    // // root.render(h2);
+// const parent=React.createElement("div",{id:"nested"},
+// [React.createElement("div",{id:"child"},[
+// React.createElement("h1",{},"Hello world react2"),
+// React.createElement("h2",{},"Hello world"),
+// React.createElement("li",{},"orange"),
+// React.createElement("li",{},"Grapes"),
+// ]),
+
+// // React.createElement("div",{id:"child2"},
+// //     [React.createElement("h1",{},"Hello world react2"),
+// //     React.createElement("h2",{},"Hello world"),
+// //     ]),
+// // ]);
+
+// const parent=React.createElement("div",{id:"parent"},
+//     React.createElement("div",{},[
+//         React.createElement("h1",{id:"heading"},"Hello welcome to world of react"),
+//         React.createElement("h2",{},"Hey you be curious to learn react")
+//     ]),
+
+//     [React.createElement("div",{id:"child"},
+//   React.createElement("h1",{},"This is Heading"),
+//     React.createElement("h2",{},"Hello Hiii")),
+// ]);
+
+
+
+
+    
+
+// ;
+
+// const root=ReactDOM.createRoot(document.getElementById("root"));
+// root.render(parent);
+// by js
+
+const heading=document.createElement("h1");
+heading.innerHTML="Hello by JS";
+const root1=document.getElementById("root");
+root.appendChild(heading);
